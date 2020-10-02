61e77df141dc2ae7ebc6974bf71107d204f06cfa
estipen <32858832+estipen@users.noreply.github.com>
2020-10-02 16:13:20 +0800

    ready for presentation

--- Banking-App.js
+++ Banking-App.js
@@ -16,9 +16,9 @@ const sendName = document.getElementById('sendName');
 const userInfo = document.getElementsByClassName('user-list');
 
 const loginInfo = function(username,password,money){
-this.username = username;
-this.password = password;
-this.money = money;
+    this.username = username;
+    this.password = password;
+    this.money = money;
 };
 
 const users = [];
@@ -28,9 +28,8 @@ c=0; //current user
 // == Button functions start here =====================================================================
 
 signupButton.onclick = function() {
-    
     if(userEmpty(userCreate) && passwordEmpty(passwordCreate) && passwordMatch(passwordCreate,passwordConfirm)){
-        if(userAlreadyCreated(users,i,userCreate)) {
+        if (!userAlreadyCreated(users,i,userCreate)) {
             users[i] = new loginInfo(userCreate.value, passwordCreate.value, 0);
             addUser(users,i,userList);
             i++;
@@ -41,14 +40,12 @@ signupButton.onclick = function() {
 
 loginButton.onclick = function() {
 
-    if(userEmpty(username) && passwordEmpty(password) && userExists(i,username,password,users)){
+    if(userEmpty(username)  && userExists(i,username,password,users) && passwordEmpty(password)){
         userDisplay.textContent = username.value;
         moneyDisplay.textContent = getMoney(i,username,users);
         let status = document.getElementById('loginSignupContainer');
-        console.log(status.className);
         status.className += ' hidden';
         status = document.getElementById('displayInfoContainer');
-        console.log(status.className);
         status.className = 'display-info-container';
     }
     clearInput();
@@ -60,20 +57,16 @@ logoutButton.onclick = function() {
     moneyDisplay.textContent = '';
 
     let status = document.getElementById('displayInfoContainer');
-    console.log(status.className);
     status.className += ' hidden';
     status = document.getElementById('loginSignupContainer');
-    console.log(status.className);
     status.className = 'login-signup-container';
 }
 
 registerButton.onclick = function() {
 
     let status = document.getElementById('loginPage');
-    console.log(status.className);
     status.className += ' hide';
     status = document.getElementById('signupPage');
-    console.log(status.className);
     status.className = 'signup-info';
     
 }
@@ -81,10 +74,8 @@ registerButton.onclick = function() {
 loginPageButton.onclick = function() {
 
     let status = document.getElementById('signupPage');
-    console.log(status.className);
     status.className += ' hide';
     status = document.getElementById('loginPage');
-    console.log(status.className);
     status.className = 'login-info';
 
 }
@@ -101,9 +92,11 @@ depositButton.onclick = function() {
 withdrawButton.onclick = function() {
     
     c=getUser(i,c,users);
-    users[c].money -= parseFloat(withdrawAmount.value);
-    moneyDisplay.textContent = users[c].money;
-    updateMoney(c,users);
+    if(enoughMoney(users,c,withdrawAmount)){
+        users[c].money -= parseFloat(withdrawAmount.value);
+        moneyDisplay.textContent = users[c].money;
+        updateMoney(c,users);
+    }
     clearInput();
 }
 
@@ -111,15 +104,19 @@ sendButton.onclick = function() {
     let sendTo = 0;   
 
     c=getUser(i,c,users);
-    for(sendTo=0;sendTo<i;sendTo++){
-        if(sendName.value === users[sendTo].username){
-            users[c].money -= parseFloat(sendAmount.value);
-            updateMoney(c,users);
-            users[sendTo].money += parseFloat(sendAmount.value);
-            updateMoney(sendTo,users);
+    if(enoughMoney(users,c,sendAmount)){
+        for(sendTo=0;sendTo<i;sendTo++){
+            if(sendName.value === users[sendTo].username){
+                users[c].money -= parseFloat(sendAmount.value);
+                updateMoney(c,users);
+                users[sendTo].money += parseFloat(sendAmount.value);
+                updateMoney(sendTo,users);
+                document.getElementById('errorsendName').innerHTML = "";
+            }  
         }
+        document.getElementById('errorsendName').innerHTML = "User does not exists.";
+        moneyDisplay.textContent = users[c].money;
     }
-    moneyDisplay.textContent = users[c].money;
     clearInput();
 }
 
@@ -142,10 +139,15 @@ function userExists(i,username,password,users) {
     for(c=0;c<i;c++){
         if(username.value === users[c].username){
             if(password.value === users[c].password) {
+                document.getElementById('error'+password.id).innerHTML = "";
                 return 1;
+            } else {
+                document.getElementById('error'+password.id).innerHTML = "Username/password is incorrect.";
+                return 0;
             }
         }
     }
+    document.getElementById('error'+password.id).innerHTML = "Username/password is incorrect.";
     return 0;
 }
     //getting money value of confirmed user logging in
@@ -171,6 +173,7 @@ function addUser(users,i,userList) {
     newPassword.className = "new-password";
 
     newMoney.id = "newMoney"+[i];
+    // newUser.id = "newUser"+[i];
 
     userList.appendChild(newList);
     newList.appendChild(newUser);
@@ -217,15 +220,16 @@ function userAlreadyCreated(users,i,userCreate) {
 
     for(c=0;c<i;c++){
         if(users[c].username === userCreate.value){
-            
-            return 0;
+            document.getElementById('error'+userCreate.id+'Exists').innerHTML = "User already exists.";
+            return 1;
         }
     }
-    return 1;
+    document.getElementById('error'+userCreate.id+'Exists').innerHTML = "";
+    return 0;
 }
     //input fields empty for user
 function userEmpty(user){
-    console.log(user.id);
+    
     if (user.value === ''){
         document.getElementById('error'+user.id).innerHTML = "Please input a valid username.";
         return 0;
@@ -251,4 +255,15 @@ function passwordMatch(passwordCreate,passwordConfirm){
     }
     document.getElementById('errorpasswordConfirm').innerHTML = "";
     return 1;
+}
+
+function enoughMoney(users,c,money){
+
+    if (users[c].money < money.value){
+        document.getElementById('error'+money.id).innerHTML = "Not enough money in bank!";
+        return 0;
+    } else {
+        document.getElementById('error'+money.id).innerHTML = "";
+        return 1;
+    }
 }
